b35ed8e4ae4e838867d79e8e95577f62ea447361
Vihanga Heshan Edirisingha <92900234+Vheshan37@users.noreply.github.com>
2026-08-27 11:21:43 +0530

    feat: add owner_name, staff_count, and management_method columns to saloon_requests table

---
+++ drizzle/0007_rainy_scalphunter.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "saloon_requests" ADD COLUMN "owner_name" text;--> statement-breakpoint
+ALTER TABLE "saloon_requests" ADD COLUMN "staff_count" text;--> statement-breakpoint
+ALTER TABLE "saloon_requests" ADD COLUMN "management_method" text;
